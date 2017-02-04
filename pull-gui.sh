709eb3d4cc709e98ec929561ae36855dc1afa797
pbca26 <pbca26@gmail.com>
2017-02-04 17:39:29 +0300

    added pull gui bash script

---
+++ pull-gui.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+if [ -d "tmp" ]; then
+  cd tmp
+else
+  mkdir tmp
+  cd tmp
+fi
+
+if [ -d "EasyDEX-GUI" ]; then
+  cd EasyDEX-GUI
+  git pull
+  cd ../
+else
+  git clone https://github.com/SuperNETorg/EasyDEX-GUI.git
+fi
+
+if [ -d "Iguana-application" ]; then
+  cd Iguana-application
+  git checkout 0.3
+  git pull
+else
+  git clone https://github.com/SuperNETorg/Iguana-application.git -b 0.3
+  cd Iguana-application
+fi
+
+bower install
+npm install
+gulp electron
+
+cd ../../
+
+rm "gui/EasyDEX-GUI" -rf
+rm "gui/Iguana-GUI" -rf
+
+cp -rf "tmp/Iguana-application/compiled/prod" "gui/Iguana-GUI"
+cp -rf "tmp/EasyDEX-GUI" "gui/EasyDEX-GUI"
