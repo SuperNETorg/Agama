0ce8a17f3450f64ebd03ae529c8bc79b58506235
Satinder Grewal <grewal.satinder@gmail.com>
2017-02-05 00:48:24 +1300

    refresh basilisk connections action added to loading scree, with new html/css/js animation

--- gui/loading.js
+++ gui/loading.js
@@ -53,3 +53,75 @@ function StartIguana() {
         }
     });
 }
+
+function EDEX_DEXnotarychains() {
+    var result = [];
+
+    var tmpIguanaRPCAuth = 'tmpIgRPCUser@'+sessionStorage.getItem('IguanaRPCAuth');
+    var ajax_data = {'userpass':tmpIguanaRPCAuth,"agent":"dpow","method":"notarychains"}
+    console.log(ajax_data);
+    $.ajax({
+        async: false,
+        type: 'POST',
+        data: JSON.stringify(ajax_data),
+        url: 'http://127.0.0.1:7778',
+        //dataType: 'text',
+        success: function(data, textStatus, jqXHR) {
+            var AjaxOutputData = JSON.parse(data);
+            //console.log('== EDEX_DEXnotarychains Data OutPut ==');
+            //console.log(AjaxOutputData);
+            result.push(AjaxOutputData);
+        },
+        error: function(xhr, textStatus, error) {
+            console.log(xhr.statusText);
+            if ( xhr.readyState == 0 ) {
+                Iguana_ServiceUnavailable();
+            }
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+    //console.log(result);
+    return result[0];
+}
+
+
+function EDEX_DEXgetinfoAll() {
+    var result = [];
+
+    var get_dex_notarychains = EDEX_DEXnotarychains();
+    console.log(get_dex_notarychains.length)
+    
+    $.each(get_dex_notarychains, function( coin_index, coin_value ) {
+        console.log(coin_index + ': ' + coin_value);
+        var tmpIguanaRPCAuth = 'tmpIgRPCUser@'+sessionStorage.getItem('IguanaRPCAuth');
+        var ajax_data = {'userpass':tmpIguanaRPCAuth,"agent":"dex","method":"getinfo","symbol":coin_value}
+        //console.log(ajax_data);
+        $.ajax({
+            async: false,
+            type: 'POST',
+            data: JSON.stringify(ajax_data),
+            url: 'http://127.0.0.1:7778',
+            //dataType: 'text',
+            success: function(data, textStatus, jqXHR) {
+                var AjaxOutputData = JSON.parse(data); //Ajax output gets the whole list of unspent coin with addresses
+                //console.log('== EDEX_DEXgetinfoAll Data OutPut ==');
+                console.log(AjaxOutputData);
+                result.push(AjaxOutputData);
+                //if (AjaxOutputData.error === 'less than required responses') {
+                //}
+            },
+            error: function(xhr, textStatus, error) {
+                console.log(xhr.statusText);
+                if ( xhr.readyState == 0 ) {
+                    Iguana_ServiceUnavailable();
+                }
+                console.log(textStatus);
+                console.log(error);
+            }
+        });
+    });
+
+    //console.log(result);
+    return result[0];
+}
