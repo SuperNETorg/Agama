3860a36aaf06a9b3de62a51f676b4a09d45e6bc4
pbca26 <pbca26@gmail.com>
2017-02-15 08:47:40 +0300

    added missing gui/init.js script

---
+++ gui/init.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $('#pulse').jRoll({
+    radius: 100,
+    animation: 'pulse'
+  });
+
+  var appConf = GetAppConf();
+  const remote = require('electron').remote;
+  var window = remote.getCurrentWindow();
+
+  $('#loading_status_text').text('Starting Iguana daemon...');
+
+  if (appConf && !appConf.manualIguanaStart) {
+    StartIguana();
+  }
+
+  if (appConf && !appConf.skipBasiliskNetworkCheck) {
+    var portcheck;
+
+    function sartcheck() {
+      portcheck = setInterval(function(){
+        Iguana_activehandle().then(function(result){
+          console.log(result);
+
+          if (result !== 'error') {
+            stopcheck();
+            $('#loading_status_text').text('Connecting to Basilisk Network...');
+            EDEX_DEXgetinfoAll();
+          }
+        })
+        //var check = Iguana_activehandle();
+        //console.log(check[0])
+      }, 2000);
+    }
+
+    function stopcheck() {
+      clearInterval(portcheck);
+    }
+
+    sartcheck();
+    //setTimeout(function(){ window.close(); }, 15000);
+  } else {
+    window.close();
+  }
+});
