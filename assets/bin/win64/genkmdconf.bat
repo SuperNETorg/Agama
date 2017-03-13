ee87105460f0973f2111e4e3037dc7c3d53f8d9a
Satinder Grewal <grewal.satinder@gmail.com>
2017-03-14 09:35:11 +1300

    latest updated komodo binaries and komodo.conf gen batch file

---
+++ assets/bin/win64/genkmdconf.bat
@@ -0,0 +1,18 @@
+mkdir %AppData%\Komodo
+
+@echo off
+
+IF NOT EXIST %AppData%\Komodo\komodo.conf (
+   (
+    echo rpcuser=kmdusr%random%%random%
+    echo rpcpashword=kmdpass%random%%random%
+    echo rpcbind=127.0.0.1
+	echo txindex=1
+	echo addnode=5.9.102.210
+	echo addnode=78.47.196.146
+	echo addnode=178.63.69.164
+	echo addnode=88.198.65.74
+	echo addnode=5.9.122.241
+	echo addnode=144.76.94.38
+    ) > %AppData%\Komodo\komodo.conf
+)
