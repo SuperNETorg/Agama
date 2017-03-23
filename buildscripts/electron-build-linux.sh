a45de449b7b497cf94371561ca1e1367926c5e7c
michal maxian <michal@maxian.sk>
2017-03-23 23:51:27 +0100

    IGUANA_VERSION to AGAMA_VERSION changed

--- buildscripts/electron-build-linux.sh
+++ buildscripts/electron-build-linux.sh
@@ -2,15 +2,15 @@
 ### Build script for Iguana application for Linux x32 and x64 platform.
 ### Created by mmaxian, 3/2017
 
-[ -z $IGUANA_VERSION ] && echo "IGUANA_VERSION variable is not set." && exit 0
+[ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
 
 echo "Build script for Iguana application for Linux x32 and x64 platform."
-echo "Preparing electron package $IGUANA_VERSION" 
+echo "Preparing electron package $AGAMA_VERSION" 
 electron-packager . --platform=linux --arch=ia32 \
   --icon=assets/icons/iguana_app_icon_png/128x128.png \
-  --out=build/ --buildVersion=$IGUANA_VERSION \
+  --out=build/ --buildVersion=$AGAMA_VERSION \
   --ignore=assets/bin/win64 --ignore=assets/bin/osx --overwrite 
 electron-packager . --platform=linux --arch=x64 \
   --icon=assets/icons/iguana_app_icon_png/128x128.png \
-  --out=build/ --buildVersion=$IGUANA_VERSION \
+  --out=build/ --buildVersion=$AGAMA_VERSION \
   --ignore=assets/bin/win64 --ignore=assets/bin/osx --overwrite 
