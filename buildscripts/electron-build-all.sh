89d190d2636508c2d893f1354a54fd3c234bb0ce
Satinder Grewal <grewal.satinder@gmail.com>
2017-03-24 13:03:19 +1300

    Revert "IGUANA_VERSION to AGAMA_VERSION changed"
    This reverts commit 4cd1bfc10b1863b5ede1f2f6a56e53d88b58e119.

--- buildscripts/electron-build-all.sh
+++ buildscripts/electron-build-all.sh
@@ -2,16 +2,16 @@
 ### Build script for Iguana application for Linux x32 and x64 platform.
 ### Created by mmaxian, 3/2017
 
-[ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
+[ -z $IGUANA_VERSION ] && echo "IGUANA_VERSION variable is not set." && exit 0
 
-echo "Preparing electron package $AGAMA_VERSION for Linux." 
+echo "Preparing electron package $IGUANA_VERSION for Linux." 
 ./buildscripts/electron-build-linux.sh
 echo
 
-echo "Preparing electron package $AGAMA_VERSION for Windows."  
+echo "Preparing electron package $IGUANA_VERSION for Windows."  
 ./buildscripts/electron-build-win.sh 
 echo
 
-echo "Preparing electron package $AGAMA_VERSION for MacOS." 
+echo "Preparing electron package $IGUANA_VERSION for MacOS." 
 ./buildscripts/electron-build-osx.sh 
 echo
