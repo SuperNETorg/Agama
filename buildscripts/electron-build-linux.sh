4d4e8ef00c3bfd6059549bb4826e3424a1a71637
Satinder Grewal <grewal.satinder@gmail.com>
2017-03-24 13:03:26 +1300

    Revert "IGUANA_VERSION to AGAMA_VERSION changed"
    This reverts commit a45de449b7b497cf94371561ca1e1367926c5e7c.

--- buildscripts/electron-build-linux.sh
+++ buildscripts/electron-build-linux.sh
@@ -2,15 +2,15 @@
 ### Build script for Iguana application for Linux x32 and x64 platform.
 ### Created by mmaxian, 3/2017
 
-[ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
+[ -z $IGUANA_VERSION ] && echo "IGUANA_VERSION variable is not set." && exit 0
 
 echo "Build script for Iguana application for Linux x32 and x64 platform."
-echo "Preparing electron package $AGAMA_VERSION" 
+echo "Preparing electron package $IGUANA_VERSION" 
 electron-packager . --platform=linux --arch=ia32 \
   --icon=assets/icons/iguana_app_icon_png/128x128.png \
-  --out=build/ --buildVersion=$AGAMA_VERSION \
+  --out=build/ --buildVersion=$IGUANA_VERSION \
   --ignore=assets/bin/win64 --ignore=assets/bin/osx --overwrite 
 electron-packager . --platform=linux --arch=x64 \
   --icon=assets/icons/iguana_app_icon_png/128x128.png \
-  --out=build/ --buildVersion=$AGAMA_VERSION \
+  --out=build/ --buildVersion=$IGUANA_VERSION \
   --ignore=assets/bin/win64 --ignore=assets/bin/osx --overwrite 
--- buildscripts/electron-build-osx.sh
+++ buildscripts/electron-build-osx.sh
@@ -2,11 +2,11 @@
 ### Build script for Iguana application for MacOS platform.
 ### Created by mmaxian, 3/2017
 
-[ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
+[ -z $IGUANA_VERSION ] && echo "IGUANA_VERSION variable is not set." && exit 0
 
 echo "Build script for Iguana application for MacOS platform."
-echo "Preparing electron package $AGAMA_VERSION"
+echo "Preparing electron package $IGUANA_VERSION"
 electron-packager . --platform=darwin --arch=x64 \
   --icon=assets/icons/iguana_app_icon.icns \
-  --out=build/ --buildVersion=$AGAMA_VERSION \
+  --out=build/ --buildVersion=$IGUANA_VERSION \
   --ignore=assets/bin/win64 --ignore=assets/bin/linux64 --overwrite
