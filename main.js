828fd8fad7c2b25614ed448e94f0eca014a0b9f0
Satinder Grewal <grewal.satinder@gmail.com>
2017-04-03 18:49:48 +1200

    version update to 2e

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var express = require('express'),
 Promise = require('bluebird');
 
 app.setName('Agama');
-app.setVersion('0.1.6.1e-beta');
+app.setVersion('0.1.6.2e-beta');
 
 if (os.platform() === 'linux') {
 	process.env.ELECTRON_RUN_AS_NODE = true;
