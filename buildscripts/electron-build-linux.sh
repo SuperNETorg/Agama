ccb21760c73563c20187fe5f85ff7513e1504f9a
michal maxian <michal@maxian.sk>
2017-04-21 02:06:07 +0200

    buildscripts for electron changed - new artifact download added

--- buildscripts/electron-build-linux.sh
+++ buildscripts/electron-build-linux.sh
@@ -4,10 +4,24 @@
 
 [ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
 
+echo
+echo =========================================
+echo Step: Removing old binaries
+cd build
+rm -rvf artifacts.supernet.org
+echo
+echo Step: Cloning latest binaries for build
+wget --recursive --no-parent https://artifacts.supernet.org/latest/linux/
+cd ..
+echo =========================================
+echo
+
 echo "Build script for Iguana application for Linux x64 platform."
 echo "Preparing electron package $AGAMA_VERSION" 
 
 electron-packager . --platform=linux --arch=x64 \
   --icon=assets/icons/iguana_app_icon_png/128x128.png \
   --out=build/ --buildVersion=$AGAMA_VERSION \
-  --ignore=assets/bin/win64 --ignore=assets/bin/osx --overwrite 
+  --ignore=build/artifacts.supernet.org/latest/windows \
+  --ignore=build/artifacts.supernet.org/latest/osx \
+  --overwrite 
