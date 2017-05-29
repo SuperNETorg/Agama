8e3044a00f9a0c72389a5b122baedea11164ab06
michal maxian <michal@maxian.sk>
2017-05-29 23:58:29 +0200

    removed copying of files to assets/bin only permission changed is in binary download

--- binary_artifacts.sh
+++ binary_artifacts.sh
@@ -16,19 +16,19 @@ echo =========================================
 echo
 pwd 
 echo =========================================
-echo Step: Copying osx binaries from artifacts to assets/bin/osx/
+echo Step: Permission +x for OSX binaries from artifacts to assets/bin/osx/
 echo 
 chmod +x assets/artifacts.supernet.org/latest/osx/iguana \
   assets/artifacts.supernet.org/latest/osx/komodo*
-cp -rvf assets/artifacts.supernet.org/latest/osx/* assets/bin/osx/
-echo 
-echo =========================================
-echo Step: Copying Win64 binaries from artifacts to assets/bin/win64/
-echo 
+#cp -rvf assets/artifacts.supernet.org/latest/osx/* assets/bin/osx/
+#echo 
+#echo =========================================
+#echo Step: Copying Windows binaries from artifacts to assets/bin/win64/
+#echo 
 #cp -rvf assets/artifacts.supernet.org/latest/windows/* assets/bin/win64/
 echo 
 echo =========================================
-echo Step: Copying linux64 binaries from artifacts to assets/bin/linux64
+echo Step: Permissions +x for linux64 binaries from artifacts to assets/bin/linux64
 echo 
 chmod +x assets/artifacts.supernet.org/latest/linux/iguana \
   assets/artifacts.supernet.org/latest/linux/komodo*
