abc31ae195334083c1b399e5eed2a1135cd4aa74
michal maxian <michal@maxian.sk>
2017-05-29 17:53:56 +0200

    download procedure removed from electron-build scripts

--- buildscripts/electron-build-linux.sh
+++ buildscripts/electron-build-linux.sh
@@ -5,17 +5,6 @@
 [ -z $AGAMA_VERSION ] && echo "AGAMA_VERSION variable is not set." && exit 0
 
 echo
-echo =========================================
-echo Step: Removing old binaries
-rm -rvf artifacts.supernet.org
-echo
-echo Step: Cloning latest binaries for build
-wget --recursive --no-parent -q https://artifacts.supernet.org/latest/linux/
-find artifacts.supernet.org/latest/linux/ -exec ls -l {} \;
-cd ..
-echo =========================================
-echo
-
 echo "Build script for Iguana application for Linux x64 platform."
 echo "Preparing electron package $AGAMA_VERSION" 
 
