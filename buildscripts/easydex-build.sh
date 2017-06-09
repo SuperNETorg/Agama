3943172aeae14b87c644ca9f60cfb5381d130e1f
michal maxian <michal@maxian.sk>
2017-06-09 08:29:14 +0200

    Adding script to build EasyDEX-GUI

---
+++ buildscripts/easydex-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to build gui for Agama App
+
+[ -d ${WORKSPACE}/gui/EasyDEX-GUI ] && cd ${WORKSPACE}/gui/EasyDEX-GUI
+[ -d ../gui/EasyDEX-GUI ] && cd ../gui/EasyDEX-GUI
+[ -d gui/EasyDEX-GUI ] && cd gui/EasyDEX-GUI
+
+echo "Building EasyDEX-GUI"
+echo "Actual directory is: ${PWD}"
+
+echo "Checkout to redux branch."
+git checkout redux
+git pull origin redux
+
+echo "Installing nodejs modules."
+npm install 
+npm install webpack
+
+echo "Building EasyDEX-GUI app."
+npm run build 
+echo "EasyDEX-GUI is built!"
