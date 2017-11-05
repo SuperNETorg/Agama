48a5fd59e2efccb2447ba5eb97624462e15e29fd
pbca26 <pbca26@gmail.com>
2017-11-05 12:15:22 +0300

    wif login

--- routes/shepherd/electrum/auth.js
+++ routes/shepherd/electrum/auth.js
@@ -2,11 +2,23 @@ module.exports = (shepherd) => {
   shepherd.post('/electrum/login', (req, res, next) => {
     for (let key in shepherd.electrumServers) {
       const _abbr = shepherd.electrumServers[key].abbr;
-      const { priv, pub } = shepherd.seedToWif(req.body.seed, shepherd.findNetworkObj(_abbr), req.body.iguana);
+      let keys;
+
+      if (req.body.seed.length === 52 &&
+          req.body.seed[0] === 'U' &&
+          req.body.seed.match(/^[a-zA-Z0-9]*$/)) {
+        let key = shepherd.bitcoinJS.ECPair.fromWIF(req.body.seed, shepherd.electrumJSNetworks.komodo);
+        keys = {
+          priv: key.toWIF(),
+          pub: key.getAddress(),
+        };
+      } else {
+        keys = shepherd.seedToWif(req.body.seed, shepherd.findNetworkObj(_abbr), req.body.iguana);
+      }
 
       shepherd.electrumKeys[_abbr] = {
-        priv,
-        pub,
+        priv: keys.priv,
+        pub: keys.pub,
       };
     }
 
