920dfe1ce77fc280f95188f9316b8e8e32dc4e12
ca333 <ca333@users.noreply.github.com>
2017-12-10 23:12:41 +0100

    add komodo bins fetch
    fetch komodo binaries from our CDN

--- binary_artifacts.sh
+++ binary_artifacts.sh
@@ -19,10 +19,23 @@ echo =========================================
 echo Step: Permission +x for OSX binaries from artifacts to assets/bin/osx/
 echo
 rm assets/artifacts.supernet.org/latest/osx/iguana
-chmod +x assets/artifacts.supernet.org/latest/osx/komodo*
+#chmod +x assets/artifacts.supernet.org/latest/osx/komodo*
 
 mkdir assets/bin
-mv assets/artifacts.supernet.org/latest/osx assets/bin/osx
+mkdir assets/bins/osx
+#mv assets/artifacts.supernet.org/latest/osx assets/bin/osx
+
+echo Moving komodo bins to assets/bin/osx
+wget https://supernetorg.bintray.com/binaries/kmd_osx_bins.zip
+checksum=`shasum -a 256 kmd_osx_bins.zip | awk '{ print $1 }'`
+if [ "$checksum" = "baa40ee046aa52494ea3be92be654fc61c47445277acadfd0d6c4cdc6b79bef9" ]; then
+    echo "Checksum is correct."
+    unzip kmd_osx_bins.zip
+    cp -rvf kmd_osx_bins/* assets/bin/osx/.
+  else
+    echo "Checksum is incorrect!"
+    exit 0
+fi
 
 echo Moving legacy libs to assets/bin
 wget https://supernetorg.bintray.com/misc/libs_legacy_osx.zip
@@ -35,6 +48,7 @@ if [ "$checksum" = "e9474aa243694a2d4c87fccc443e4b16a9a5172a24da76af9e5ecddd0066
     echo "Checksum is incorrect!"
     exit 0
 fi
+
 echo =========================================
 echo Step: Moving Windows binaries from artifacts to assets/bin/win64/
 #echo
